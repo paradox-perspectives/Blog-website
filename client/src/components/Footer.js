427d0f88f3d1227d0ed56c726855bbaf6203b1b1
Gabriellvl <vanlangenhovefga@gmail.com>
2024-08-03 19:54:11 +0200

    footer smaller

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,8 +3,8 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer className="w-full py-2 bg-gray-500 text-center text-white fixed bottom-0 z-50">
-            <p>
+        <footer className="w-full py-1 bg-gray-500 text-center text-white bottom-0 z-50">
+            <p className={"text-xs"}>
                 Created by <a href="https://github.com/Gabriellvl" className="text-blue-400" target="_blank" rel="noopener noreferrer">Gabriellvl</a>
             </p>
         </footer>
