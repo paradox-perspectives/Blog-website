58bbefcf375d4373105fa3f683afda08b72b2ea6
Gabriellvl <vanlangenhovefga@gmail.com>
2024-10-03 16:07:31 +0200

    google maps loader

--- client/src/components/ContactPage.js
+++ client/src/components/ContactPage.js
@@ -5,6 +5,7 @@ import whatsapp from "../whatsapp.png";
 import facebook from "../facebook.png";
 import insta from "../Insta.png";
 import {Helmet} from "react-helmet";
+import MapComponent from "./MapComponent";
 
 
 const ContactPage = () => {
@@ -63,9 +64,6 @@ const ContactPage = () => {
         );
     };
 
-
-    const googleMapUrl = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2545.593501913679!2d5.493521555017086!3d50.35548680718067!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c04f3dcfd7c73b%3A0xc2a744eb720127d2!2sOxyvitale%20Fitness!5e0!3m2!1snl!2sbe!4v1721588366495!5m2!1snl!2sbe";
-    const googleMapsRedirectUrl = "https://www.google.com/maps/place/Oxyvitale+Fitness/@50.3554252,5.4947661,17z/data=!3m1!4b1!4m6!3m5!1s0x47c04f3dcfd7c73b:0xc2a744eb720127d2!8m2!3d50.3554252!4d5.4947661!16s%2Fg%2F11f76_04p4?entry=ttu";
 
     return (
         <div>
@@ -138,27 +136,12 @@ const ContactPage = () => {
                 <SocialIcons/>
             </div>
 
-            <div className="relative mx-auto pl-1 pr-1 md:pr-32 py-2 max-w-4xl pb-6" style={{height: '450px'}}>
-                <h1 className="text-2xl font-bold mb-4">Trouvez-nous</h1>
-                <iframe
-                    src={googleMapUrl}
-                    style={{border: 0, width: '100%', height: '100%'}}
-                    allowFullScreen=""
-                    loading="lazy"
-                    title="Google Map"
-                ></iframe>
-                <a
-                    href={googleMapsRedirectUrl}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="absolute top-0 left-0 w-full h-full"
-                    style={{textDecoration: 'none'}}
-                >
-                    <span className="sr-only">Open in Google Maps</span>
-                </a>
-                <br/>
-                <br/>
-            </div>
+            <MapComponent />
+
+            <br/>
+            <br/>
+            <br/>
+            <br/>
         </div>
     );
 };
